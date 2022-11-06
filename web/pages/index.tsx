a7f03dc0c5e0fe085b7a8ba9c53cf6d044032892
Marcos Botene <marcosjbotene@gmail.com>
2022-11-06 17:33:20 -0300

    integrating the endpoint that returns the number of pools created.

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <div>Hello World</div>;
+import { GetServerSideProps } from 'next';
+
+interface HomeProps {
+  count: number;
 }
+
+export default function Home(props: HomeProps) {
+  return <h1>Contagem: {props.count}</h1>;
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count');
+
+  const data = await response.json();
+
+  return {
+    props: {
+      count: data.count,
+    },
+  };
+};
