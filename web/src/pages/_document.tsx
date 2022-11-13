9ecb9be797b0433d5304fffcce483e8680bea6df
Marcos Botene <marcosjbotene@gmail.com>
2022-11-12 22:16:29 -0300

    added an image background to the page.

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -16,7 +16,7 @@ export default function Document() {
         />
       </Head>
 
-      <body>
+      <body className='bg-gray-900 bg-app bg-no-repeat bg-cover'>
         <Main />
         <NextScript />
       </body>
