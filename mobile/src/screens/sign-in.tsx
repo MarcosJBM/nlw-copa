2364f0987cec498e535339dea4d73bc28e382461
Marcos Botene <marcosjbotene@gmail.com>
2022-11-15 19:26:30 -0300

    added the content of sign in screen.

--- mobile/src/screens/sign-in.tsx
+++ mobile/src/screens/sign-in.tsx
@@ -1,10 +1,24 @@
-import { Center, Text } from 'native-base';
+import { Fontisto } from '@expo/vector-icons';
+import { Center, Icon, Text } from 'native-base';
+
+import Logo from '../assets/logo.svg';
+import { Button } from '../components';
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor='gray.900'>
-      <Text color='white' fontSize={24} fontFamily='heading'>
-        Sign In
+    <Center flex='1' bgColor='gray.900' p='7'>
+      <Logo width='212' height='40' />
+
+      <Button
+        title='ENTRAR COM GOOGLE'
+        type='SECONDARY'
+        leftIcon={<Icon as={Fontisto} name='google' color='white' size='md' />}
+        mt='12'
+      />
+
+      <Text color='white' textAlign='center' mt='4'>
+        Não utilizamos nenhuma informação além {'\n'}
+        do seu e-mail para criação de sua conta.
       </Text>
     </Center>
   );
