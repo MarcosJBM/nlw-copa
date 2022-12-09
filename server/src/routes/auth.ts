8ea0720c55d9a30b1a2676ff4ca119e6d42c69b6
Marcos Botene <marcosjbotene@gmail.com>
2022-12-08 23:05:18 -0300

    communicating with google's oauth2 api.

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,7 +1,34 @@
 import { FastifyInstance } from 'fastify';
-
-import { prisma } from '../lib';
+import { z } from 'zod';
 
 export async function authRoutes(fastify: FastifyInstance) {
-  return;
+  fastify.post('/users', async request => {
+    const createUserBody = z.object({
+      access_token: z.string(),
+    });
+
+    const { access_token } = createUserBody.parse(request.body);
+
+    const userResponse = await fetch(
+      'https://www.googleapis.com/oauth2/v2/userinfo',
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+
+    const userData = await userResponse.json();
+
+    const userInfoSchema = z.object({
+      id: z.string(),
+      email: z.string().email(),
+      name: z.string(),
+      picture: z.string().url(),
+    });
+
+    const userInfo = userInfoSchema.parse(userData);
+
+    return { userInfo };
+  });
 }
