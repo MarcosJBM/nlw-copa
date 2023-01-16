3087404cf19dc6ac7c9088a87833d59d5e732c7d
Marcos Botene <marcosjbotene@gmail.com>
2023-01-15 22:47:59 -0300

    created route to get user data.

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,8 +2,15 @@ import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 
 import { prisma } from '../libs';
+import { authenticate } from '../plugins';
 
 export async function authRoutes(fastify: FastifyInstance) {
+  fastify.get('/me', { onRequest: [authenticate] }, async request => {
+    await request.jwtVerify();
+
+    return { user: request.user };
+  });
+
   fastify.post('/users', async request => {
     const createUserBody = z.object({
       access_token: z.string(),
