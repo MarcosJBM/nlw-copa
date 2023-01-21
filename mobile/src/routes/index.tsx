6d1ce9444070e743630cb565054e0c052553bacb
Marcos Botene <marcosjbotene@gmail.com>
2023-01-21 17:28:21 -0300

    displaying different routes if the user has logged in.

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,12 +1,18 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { Box } from 'native-base';
 
+import { useAuth } from '../hooks';
 import { SignIn } from '../screens';
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
+  const { user } = useAuth();
+
   return (
-    <NavigationContainer>
-      <SignIn />
-    </NavigationContainer>
+    <Box flex={1} bg='gray.900'>
+      <NavigationContainer>
+        {user.name ? <AppRoutes /> : <SignIn />}
+      </NavigationContainer>
+    </Box>
   );
 }
