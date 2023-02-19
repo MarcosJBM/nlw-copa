a116e97a6c52f6f0cfa8b46be23a5590c3835e4a
Marcos Botene <marcosjbotene@gmail.com>
2023-02-19 18:30:52 -0300

    wip.

--- mobile/src/components/empty-my-pool-list.tsx
+++ mobile/src/components/empty-my-pool-list.tsx
@@ -1,17 +1,24 @@
 import { Pressable, Row, Text } from 'native-base';
+import { Share } from 'react-native';
 
 interface Props {
   code: string;
 }
 
 export function EmptyMyPoolList({ code }: Props) {
+  async function handleCodeShare() {
+    await Share.share({
+      message: code,
+    });
+  }
+
   return (
     <Row flexWrap='wrap' justifyContent='center' p={4}>
       <Text color='gray.200' fontSize='sm'>
         Esse bolão ainda não tem participantes, que tal
       </Text>
 
-      <Pressable onPress={() => {}}>
+      <Pressable onPress={handleCodeShare}>
         <Text
           textDecorationLine='underline'
           color='yellow.500'
