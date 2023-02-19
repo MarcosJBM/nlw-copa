f8a82f5bd50d7127c0635b877b55db2facd11cc7
Marcos Botene <marcosjbotene@gmail.com>
2023-02-19 17:44:42 -0300

    created screen to list all guesses.

--- mobile/src/components/guesses.tsx
+++ mobile/src/components/guesses.tsx
@@ -1,9 +1,95 @@
-import { Box } from 'native-base';
+import { FlatList, useToast } from 'native-base';
+import { useEffect, useState } from 'react';
+
+import { api } from '../services/api';
+import { EmptyMyPoolList } from './empty-my-pool-list';
+import { Game, GameProps } from './game';
+import { Loading } from './loading';
 
 interface Props {
   poolId: string;
+  code: string;
 }
 
-export function Guesses({ poolId }: Props) {
-  return <Box></Box>;
+export function Guesses({ poolId, code }: Props) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [games, setGames] = useState<GameProps[]>([]);
+  const [firstTeamPoints, setFirstTeamPoints] = useState<string>('');
+  const [secondTeamPoints, setSecondTeamPoints] = useState<string>('');
+
+  const toast = useToast();
+
+  async function fetchGames() {
+    try {
+      setIsLoading(true);
+
+      const response = await api.get(`/pools/${poolId}/games`);
+
+      setGames(response.data.games);
+    } catch (error) {
+      toast.show({
+        title: 'Não foi possível listar os jogos',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function handleGuessConfirm(gameId: string) {
+    try {
+      if (!firstTeamPoints.trim() || !secondTeamPoints.trim()) {
+        return toast.show({
+          title: 'Informe o placar para palpitar',
+          placement: 'top',
+          bgColor: 'red.500',
+        });
+      }
+
+      await api.post(`/pools/${poolId}/games/${gameId}/guesses`, {
+        firstTeamPoints: Number(firstTeamPoints),
+        secondTeamPoints: Number(secondTeamPoints),
+      });
+
+      toast.show({
+        title: 'Palpite realizado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500',
+      });
+
+      fetchGames();
+    } catch (error) {
+      console.log(error);
+
+      toast.show({
+        title: 'Não foi possível enviar o palpite',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+  }
+
+  useEffect(() => {
+    fetchGames();
+  }, [poolId]);
+
+  if (isLoading) return <Loading />;
+
+  return (
+    <FlatList
+      data={games}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => (
+        <Game
+          data={item}
+          setFirstTeamPoints={setFirstTeamPoints}
+          setSecondTeamPoints={setSecondTeamPoints}
+          onGuessConfirm={() => handleGuessConfirm(item.id)}
+        />
+      )}
+      _contentContainerStyle={{ pb: 10 }}
+      ListEmptyComponent={() => <EmptyMyPoolList code={code} />}
+    />
+  );
 }
